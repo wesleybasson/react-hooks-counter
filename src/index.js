c56dea65a00f0a0dd5833bd6a45e09ad834fe0b3
Wesley Basson <basso.we@gmail.com>
2019-08-09 11:40:54 +0200

    Create simple React app using hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// originally we had { count: 0, name: '' } for state.
+
+const App = ({ countIn = 0 }) => {
+    const [count, setCount] =  useState(countIn)
+    const increment = () => {
+        setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count - 1)
+    }
+    const reset = () => {
+        setCount(countIn)
+    }
+    return (
+        <div>
+            <p>The current count is {count}</p>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={reset}>reset to {countIn}</button>
+        </div>
+    );
+}
+
+ReactDOM.render(<App countIn={0} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
